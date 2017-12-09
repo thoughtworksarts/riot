cb994bdcc4392e449d48050bb777c0daca1cf2e1
ElderMael <sftwr.mael@gmail.com>
2017-12-09 13:33:52 -0600

    Make Webcam Image Use Highest Resolution Available

--- src/test/java/io/toughtworksarts/riot/webcam/WebCamFeedTest.java
+++ src/test/java/io/toughtworksarts/riot/webcam/WebCamFeedTest.java
@@ -1,11 +1,17 @@
 package io.toughtworksarts.riot.webcam;
 
 import com.github.sarxos.webcam.Webcam;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,15 +22,50 @@ public class WebCamFeedTest {
     public void shouldBeAbleToLoadImageFromWebCam() throws Exception {
         // given
         Webcam webcam = Webcam.getDefault();
-        File file = File.createTempFile("capture", "png");
+        File file = File.createTempFile("capture", ".png");
+        Dimension highestResAvailable = Arrays.asList(webcam.getViewSizes())
+                .stream()
+                .map(Tuple::fromDimension)
+                .max(Comparator.naturalOrder())
+                .map(Tuple::getDimension)
+                .orElseThrow(Exception::new);
+
 
         // when
+        webcam.setViewSize(highestResAvailable);
         webcam.open();
-        ImageIO.write(webcam.getImage(), "PNG", file);
+        BufferedImage image = webcam.getImage();
+        ImageIO.write(image, "PNG", file);
+        webcam.close();
 
         // then
         log.info("File created: '{}'", file.getCanonicalPath());
         assertThat(file).exists();
+
+        double expected = highestResAvailable.getHeight() * highestResAvailable.getWidth();
+        double actual = image.getHeight() * image.getWidth();
+
+        assertThat(expected).isEqualTo(actual);
+    }
+
+
+    @RequiredArgsConstructor
+    static class Tuple implements Comparable<Tuple> {
+
+        @Getter
+        private final Double area;
+
+        @Getter
+        private final Dimension dimension;
+
+        public static Tuple fromDimension(Dimension dimension) {
+            return new Tuple(dimension.getHeight() * dimension.getWidth(), dimension);
+        }
+
+        @Override
+        public int compareTo(Tuple o) {
+            return this.area.compareTo(o.area);
+        }
     }
 
 }
