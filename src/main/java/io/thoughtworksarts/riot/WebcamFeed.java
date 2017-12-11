3c4818ad72cb0e0d7638e2f395b85360508e42aa
ElderMael <sftwr.mael@gmail.com>
2017-12-10 18:08:07 -0600

    Create Webcam Feed And Make Sure It Uses The Highest Resolution

---
+++ src/main/java/io/thoughtworksarts/riot/WebcamFeed.java
@@ -0,0 +1,92 @@
+package io.thoughtworksarts.riot;
+
+
+import com.github.sarxos.webcam.Webcam;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
+
+public class WebcamFeed {
+
+    private final Webcam webcam;
+
+    public WebcamFeed() {
+        this.webcam = Webcam.getDefault();
+        Dimension highestResAvailable = getHighestResolutionAvailable();
+        this.webcam.setViewSize(highestResAvailable);
+    }
+
+
+    public File takePicture() {
+
+        try (WebcamAutoCloseableAdapter webcamAdapter = new WebcamAutoCloseableAdapter(this.webcam)) {
+
+            File file = File.createTempFile("capture", ".png");
+
+            BufferedImage image = webcamAdapter.getImage();
+
+            ImageIO.write(image, "PNG", file);
+
+            return file;
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to take picture: " + e.getMessage(), e);
+        }
+    }
+
+    public Dimension getHighestResolutionAvailable() {
+        return Arrays.stream(webcam.getViewSizes())
+                .map(WebcamAndResolutionPair::fromDimension)
+                .max(Comparator.naturalOrder())
+                .map(WebcamAndResolutionPair::getDimension)
+                .orElseThrow(RuntimeException::new);
+    }
+
+
+    private class WebcamAutoCloseableAdapter implements AutoCloseable {
+
+        private final Webcam webcam;
+
+        WebcamAutoCloseableAdapter(Webcam webcam) {
+            this.webcam = webcam;
+            this.webcam.open();
+        }
+
+        public BufferedImage getImage() {
+            return this.webcam.getImage();
+        }
+
+        @Override
+        public void close() throws Exception {
+            webcam.close();
+        }
+    }
+
+    @RequiredArgsConstructor
+    private static class WebcamAndResolutionPair implements Comparable<WebcamAndResolutionPair> {
+
+        @Getter
+        private final Double area;
+
+        @Getter
+        private final Dimension dimension;
+
+        static WebcamAndResolutionPair fromDimension(Dimension dimension) {
+            return new WebcamAndResolutionPair(dimension.getHeight() * dimension.getWidth(), dimension);
+        }
+
+        @Override
+        public int compareTo(WebcamAndResolutionPair o) {
+            Objects.requireNonNull(o);
+
+            return this.area.compareTo(o.area);
+        }
+    }
+
+}
--- src/test/java/io/toughtworksarts/riot/webcam/WebCamFeedTest.java
+++ src/test/java/io/toughtworksarts/riot/webcam/WebCamFeedTest.java
@@ -1,17 +1,13 @@
 package io.toughtworksarts.riot.webcam;
 
 import com.github.sarxos.webcam.Webcam;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import io.thoughtworksarts.riot.WebcamFeed;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.Arrays;
-import java.util.Comparator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,53 +15,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WebCamFeedTest {
 
     @Test
+    public void shouldGetHighestResolutionAvailable() {
+
+        // given
+        WebcamFeed webcamFeed = new WebcamFeed();
+
+        // when
+        Dimension highestResolutionAvailable = webcamFeed.getHighestResolutionAvailable();
+        double actual = highestResolutionAvailable.getHeight() * highestResolutionAvailable.getWidth();
+
+        // then
+        Arrays.asList(Webcam.getDefault().getViewSizes())
+                .stream()
+                .map(this::toArea)
+                .allMatch(area -> area <= actual);
+    }
+
+    @Test
     public void shouldBeAbleToLoadImageFromWebCam() throws Exception {
         // given
-        Webcam webcam = Webcam.getDefault();
-        File file = File.createTempFile("capture", ".png");
-        Dimension highestResAvailable = Arrays.asList(webcam.getViewSizes())
-                .stream()
-                .map(Tuple::fromDimension)
-                .max(Comparator.naturalOrder())
-                .map(Tuple::getDimension)
-                .orElseThrow(Exception::new);
+        WebcamFeed webcamFeed = new WebcamFeed();
 
 
         // when
-        webcam.setViewSize(highestResAvailable);
-        webcam.open();
-        BufferedImage image = webcam.getImage();
-        ImageIO.write(image, "PNG", file);
-        webcam.close();
+        File file = webcamFeed.takePicture();
 
         // then
         log.info("File created: '{}'", file.getCanonicalPath());
-        assertThat(file).exists();
+        assertThat(file)
+                .exists();
 
-        double expected = highestResAvailable.getHeight() * highestResAvailable.getWidth();
-        double actual = image.getHeight() * image.getWidth();
-
-        assertThat(expected).isEqualTo(actual);
     }
 
-
-    @RequiredArgsConstructor
-    static class Tuple implements Comparable<Tuple> {
-
-        @Getter
-        private final Double area;
-
-        @Getter
-        private final Dimension dimension;
-
-        public static Tuple fromDimension(Dimension dimension) {
-            return new Tuple(dimension.getHeight() * dimension.getWidth(), dimension);
-        }
-
-        @Override
-        public int compareTo(Tuple o) {
-            return this.area.compareTo(o.area);
-        }
+    public double toArea(Dimension dimension) {
+        return dimension.getWidth() * dimension.getHeight();
     }
+
 
 }
