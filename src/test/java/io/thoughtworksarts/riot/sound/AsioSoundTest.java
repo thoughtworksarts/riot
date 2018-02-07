42da177c44dea205cdf102edad9c70a8dc23ef45
ellaholmes <eholmes@thoughtworks.com>
2018-02-07 11:16:44 -0500

    ignoring sound test - only works on windows

--- src/test/java/io/thoughtworksarts/riot/sound/AsioSoundTest.java
+++ src/test/java/io/thoughtworksarts/riot/sound/AsioSoundTest.java
@@ -1,12 +1,12 @@
 package io.thoughtworksarts.riot.sound;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Ignore;
 
 import java.util.concurrent.TimeUnit;
 
 public class AsioSoundTest {
 
-    @Test
+    @Ignore
     public void testThatAsioCanMakeABeep() throws InterruptedException {
         AsioSound sound = new AsioSound();
         sound.start();
