7456524bc1fc6d78ad590b5a154bf5792eda6259
angelicaperez37 <angelica.perez37@gmail.com>
2018-02-08 17:40:30 -0500

    Add setup method

--- src/test/java/io/thoughtworksarts/riot/facialrecognition/DeepLearningProcessorTest.java
+++ src/test/java/io/thoughtworksarts/riot/facialrecognition/DeepLearningProcessorTest.java
@@ -1,34 +1,37 @@
 package io.thoughtworksarts.riot.facialrecognition;
 
+import org.deeplearning4j.nn.modelimport.keras.InvalidKerasConfigurationException;
 import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+import org.deeplearning4j.nn.modelimport.keras.UnsupportedKerasConfigurationException;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class DeepLearningProcessorTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class DeepLearningProcessorTest {
+
+    private String h5File;
+    private String jsonFile;
+    private MultiLayerNetwork model;
+
+    @BeforeAll
+    public void setup() throws UnsupportedKerasConfigurationException, IOException, InvalidKerasConfigurationException {
+        h5File = this.getCompleteFileName("conv2d_weights.h5");
+        jsonFile = this.getCompleteFileName("conv2d_model.json");
+        model = KerasModelImport.importKerasSequentialModelAndWeights(jsonFile, h5File);
+        System.out.println("Here");
+    }
 
     @Test
-    void shouldBeAbleToImportKerasModel() throws Exception {
-        // given
-        String fileName = this.getCompleteFileName("testimage.png");
-        String h5File = this.getCompleteFileName("conv2d_weights.h5");
-        String jsonFile = this.getCompleteFileName("conv2d_model.json");
+    void shouldFindSameLayersWhenLoadsNetwork() {
         List<String> layerNames = Arrays.asList("convolution2d_1", "convolution2d_2", "maxpooling2d_1", "convolution2d_3", "convolution2d_4", "maxpooling2d_2", "dense_1");
-
-        File imageFile = new File(fileName);
-        BufferedImage image = ImageIO.read(imageFile);
-
-        // when
-        MultiLayerNetwork model = KerasModelImport.importKerasSequentialModelAndWeights(jsonFile, h5File);
-
-        // then
         assertEquals(layerNames, model.getLayerNames());
     }
 
@@ -39,5 +42,4 @@ class DeepLearningProcessorTest {
                 .getResource(relativePath)
                 .getFile();
     }
-
 }
