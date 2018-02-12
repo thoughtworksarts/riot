db6763ab136afa60cb047723f9d099d008914080
angelicaperez37 <angelica.perez37@gmail.com>
2018-02-12 15:34:42 -0500

    Check raw image pixels of test image are loaded correctly

--- src/test/java/io/thoughtworksarts/riot/facialrecognition/DeepLearningProcessorTest.java
+++ src/test/java/io/thoughtworksarts/riot/facialrecognition/DeepLearningProcessorTest.java
@@ -1,5 +1,6 @@
 package io.thoughtworksarts.riot.facialrecognition;
 
+import org.datavec.image.loader.ImageLoader;
 import org.deeplearning4j.nn.modelimport.keras.InvalidKerasConfigurationException;
 import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 import org.deeplearning4j.nn.modelimport.keras.UnsupportedKerasConfigurationException;
@@ -7,11 +8,16 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.nd4j.linalg.api.ndarray.INDArray;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -20,19 +26,78 @@ public class DeepLearningProcessorTest {
     private String h5File;
     private String jsonFile;
     private MultiLayerNetwork model;
+    private File imageFile;
 
     @BeforeAll
     public void setup() throws UnsupportedKerasConfigurationException, IOException, InvalidKerasConfigurationException {
         h5File = this.getCompleteFileName("conv2d_weights.h5");
         jsonFile = this.getCompleteFileName("conv2d_model.json");
         model = KerasModelImport.importKerasSequentialModelAndWeights(jsonFile, h5File);
-        System.out.println("Here");
+
+        String fileName = this.getCompleteFileName("testimage.png");
+        imageFile = new File(fileName);
     }
 
     @Test
     void shouldFindSameLayersWhenLoadsNetwork() {
         List<String> layerNames = Arrays.asList("convolution2d_1", "convolution2d_2", "maxpooling2d_1", "convolution2d_3", "convolution2d_4", "maxpooling2d_2", "dense_1");
         assertEquals(layerNames, model.getLayerNames());
+    }
+
+    @Test
+    void shouldFindSameRawPixelValuesInTestImage() throws IOException {
+        int[] expectedPixels = new int[]{255, 251, 226, 255, 251, 230, 255, 254, 236};
+
+        ImageLoader imageLoader = new ImageLoader();
+        int[][] imageData = imageLoader.fromFile(imageFile);
+        int[] actualPixels = new int[9];
+        // Only looks at first 3 pixels of 4096 pixels
+        for (int i = 0; i < 3; i++) {
+            actualPixels[i*3] = this.getRPixelValue(imageData[0][i]);
+            actualPixels[(i*3)+1] = this.getGPixelValue(imageData[0][i]);
+            actualPixels[(i*3)+2] = this.getBPixelValue(imageData[0][i]);
+        }
+        assertArrayEquals(expectedPixels, actualPixels);
+    }
+
+    private int getRPixelValue(int pixel) {
+        return (pixel >> 16) & 0xff;
+    }
+
+    private int getGPixelValue(int pixel) {
+        return (pixel >> 8) & 0xff;
+    }
+
+    private int getBPixelValue(int pixel) {
+        return (pixel) & 0xff;
+    }
+
+    private void printPixelARGB(int pixel) {
+        int alpha = (pixel >> 24) & 0xff;
+        int red = (pixel >> 16) & 0xff;
+        int green = (pixel >> 8) & 0xff;
+        int blue = (pixel) & 0xff;
+        System.out.println("argb: " + alpha + ", " + red + ", " + green + ", " + blue);
+    }
+
+    private INDArray prepareImageForNet(BufferedImage image) {
+        ImageLoader imageLoader = new ImageLoader();
+        INDArray image_array = imageLoader.asMatrix(image);
+        INDArray test_data = image_array.ravel();
+        int[] shape = new int[]{1, 1, 64, 64};
+        test_data = test_data.reshape(shape);
+        return test_data;
+    }
+
+    private static BufferedImage resize(BufferedImage img, int newW, int newH) {
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return dimg;
     }
 
     private String getCompleteFileName(String relativePath) {
