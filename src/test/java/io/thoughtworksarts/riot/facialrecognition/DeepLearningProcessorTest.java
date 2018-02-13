efe909139ca3a0f7d26c346c48d4ce6dc71c0a4f
angelicaperez37 <angelica.perez37@gmail.com>
2018-02-13 10:37:34 -0500

    Validate pixel values when converted to gray

--- src/test/java/io/thoughtworksarts/riot/facialrecognition/DeepLearningProcessorTest.java
+++ src/test/java/io/thoughtworksarts/riot/facialrecognition/DeepLearningProcessorTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.util.ArrayUtil;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -29,6 +30,8 @@ public class DeepLearningProcessorTest {
     private String jsonFile;
     private MultiLayerNetwork model;
     private File imageFile;
+    private int[][] imageData;
+    private float[] grayImageData;
 
     @BeforeAll
     public void setup() throws UnsupportedKerasConfigurationException, IOException, InvalidKerasConfigurationException {
@@ -38,6 +41,8 @@ public class DeepLearningProcessorTest {
 
         String fileName = this.getCompleteFileName("testimage.png");
         imageFile = new File(fileName);
+        ImageLoader imageLoader = new ImageLoader();
+        imageData = imageLoader.fromFile(imageFile);
     }
 
     @Test
@@ -49,9 +54,6 @@ public class DeepLearningProcessorTest {
     @Test
     void shouldFindSameRawPixelValuesInTestImage() throws IOException {
         int[] expectedPixels = new int[]{255, 251, 226, 255, 251, 230, 255, 254, 236};
-
-        ImageLoader imageLoader = new ImageLoader();
-        int[][] imageData = imageLoader.fromFile(imageFile);
         int[] actualPixels = new int[PIXEL_COUNT * RGB_CHANNEL_COUNT];
         // Only looks at first 3 pixels of 4096 pixels
         for (int pixelIndex = 0; pixelIndex < RGB_CHANNEL_COUNT; pixelIndex++) {
@@ -62,6 +64,18 @@ public class DeepLearningProcessorTest {
         assertArrayEquals(expectedPixels, actualPixels);
     }
 
+    @Test
+    void shouldFindSamePixelValuesAfterTransformingImageToGrayscale() throws IOException {
+        float[] expectedPixels = new float[]{0.980578431373f, 0.981709411765f, 0.991822352941f, 0.954831764706f, 0.849507058824f, 0.950403529412f, 1.0f, 1.0f, 1.0f, 1.0f};
+
+        ImageLoader imageLoader = new ImageLoader();
+        BufferedImage colorImage = imageLoader.toImage(imageLoader.asMatrix(imageFile));
+        grayImageData = ArrayUtil.flatten(this.toNormalizedGrayscale(colorImage));
+        float[] actualPixels = Arrays.copyOfRange(grayImageData, 0, 10);
+
+        float delta = 0.0001f;
+        assertArrayEquals(expectedPixels, actualPixels, delta);
+    }
 
     private int getRPixelValue(int pixel) {
         return (pixel >> 16) & 0xff;
@@ -81,6 +95,23 @@ public class DeepLearningProcessorTest {
         int green = (pixel >> 8) & 0xff;
         int blue = (pixel) & 0xff;
         System.out.println("argb: " + alpha + ", " + red + ", " + green + ", " + blue);
+    }
+
+    private float[][] toNormalizedGrayscale(BufferedImage colorImage) {
+        float[][] grayImageData = new float[colorImage.getWidth()][colorImage.getHeight()];
+        for (int rowIndex = 0; rowIndex < colorImage.getWidth(); ++rowIndex) {
+            for (int colIndex = 0; colIndex < colorImage.getHeight(); ++colIndex) {
+                int rgb = colorImage.getRGB(rowIndex, colIndex);
+                int r = (rgb >> 16) & 0xFF;
+                int g = (rgb >> 8) & 0xFF;
+                int b = (rgb & 0xFF);
+
+                // uses rgb to grayscale formula from Python skimage color library method rgb2gray()
+                float gray = (r * 0.2125f) + (g * 0.7154f) + (b * 0.0721f);
+                grayImageData[rowIndex][colIndex] = gray/255;
+            }
+        }
+        return grayImageData;
     }
 
     private INDArray prepareImageForNet(BufferedImage image) {
