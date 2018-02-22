2caad2808bcb329d32e7c0d96243a75df5c282ed
angelicaperez37 <angelica.perez37@gmail.com>
2018-02-22 14:20:22 -0500

    Remove webcam test

--- src/test/java/io/thoughtworksarts/riot/facialrecognition/FacialEmotionRecognitionAPITest.java
+++ src/test/java/io/thoughtworksarts/riot/facialrecognition/FacialEmotionRecognitionAPITest.java
@@ -1,25 +1,5 @@
 package io.thoughtworksarts.riot.facialrecognition;
 
-import org.junit.Ignore;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class FacialEmotionRecognitionAPITest {
 
-    @Test
-    @Ignore
-    void shouldGetEmotionValuesBetweenZeroAndOneForWebcamImage() {
-        String configPath = "src/main/resources/neuralNetConfig.json";
-        FacialEmotionRecognitionAPI facialEmotionRecognitionAPI = new FacialEmotionRecognitionAPI(configPath);
-        facialEmotionRecognitionAPI.initialise();
-
-        float calmValue = facialEmotionRecognitionAPI.getCalm();
-        float angerValue = facialEmotionRecognitionAPI.getAnger();
-        float fearValue = facialEmotionRecognitionAPI.getFear();
-
-        assertTrue(calmValue >= 0 && calmValue <= 1);
-        assertTrue(angerValue >= 0 && angerValue <= 1);
-        assertTrue(fearValue >= 0 && angerValue <= 1);
-    }
 }
