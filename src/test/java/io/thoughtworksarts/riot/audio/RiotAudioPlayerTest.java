1ef806c6335009b87a44b449476154dbdf177b7a
ellaholmes <eholmes@thoughtworks.com>
2018-02-27 15:15:51 -0500

    Attempt to fix audio test - Ella

--- src/test/java/io/thoughtworksarts/riot/audio/RiotAudioPlayerTest.java
+++ src/test/java/io/thoughtworksarts/riot/audio/RiotAudioPlayerTest.java
@@ -11,42 +11,35 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RiotAudioPlayerTest {
 
-    private RiotAudioPlayer audioPlayer;
+    @Test
+    void shouldPlayGivenFile() throws Exception {
 
-    @BeforeEach
-    void setUp() throws Exception {
-        audioPlayer = OSChecker.isWindows() ? new AudioPlayer() : new JavaSoundAudioPlayer();
+        boolean isWindows = OSChecker.isWindows();
+        RiotAudioPlayer  audioPlayer = isWindows ? new AudioPlayer() : new JavaSoundAudioPlayer();
         String wavFile = this.getClass().getClassLoader().getResource("audio/audio.wav").getFile();
         audioPlayer.initialise("ASIO4ALL v2", wavFile);
 
-    }
-
-    @Ignore //Test that the other test works on the alien ware, if so - delete this test.
-    public void asioDriverConnectorShouldPlayAudioFile() {
-        AudioPlayer audioPlayer = new AudioPlayer();
-        try {
-            AsioDriverConnector driverConnector = new AsioDriverConnector(audioPlayer);
-            driverConnector.start();
+        if( isWindows ){
+            try {
+                AsioDriverConnector driverConnector = new AsioDriverConnector((AudioPlayer) audioPlayer);
+                driverConnector.start();
+                audioPlayer.resume();
+                TimeUnit.SECONDS.sleep(3);
+                driverConnector.shutdown();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                assertTrue(false);
+            }
+        } else {
             audioPlayer.resume();
-            TimeUnit.SECONDS.sleep(3);
-            driverConnector.shutdown();
+            Thread.sleep(3000);
+            audioPlayer.pause();
+            Thread.sleep(3000);
+            audioPlayer.seek(120);
+            audioPlayer.resume();
+            Thread.sleep(3000);
         }
-        catch (Exception e) {
-            e.printStackTrace();
-            assertTrue(false);
-        }
-        audioPlayer.shutdown();
-    }
-
-    @Test
-    void shouldPlayGivenFile() throws Exception {
-        audioPlayer.resume();
-        Thread.sleep(3000);
-        audioPlayer.pause();
-        Thread.sleep(3000);
-        audioPlayer.seek(120);
-        audioPlayer.resume();
-        Thread.sleep(3000);
         audioPlayer.shutdown();
     }
 }
