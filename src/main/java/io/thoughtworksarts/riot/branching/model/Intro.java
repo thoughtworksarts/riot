8b9077d1558f11b714c565fca273a1de3452f00b
ewu <ewu@thoughtworks.com>
2018-03-07 14:44:09 -0500

    adding Intro object - MR + EmilY

---
+++ src/main/java/io/thoughtworksarts/riot/branching/model/Intro.java
@@ -0,0 +1,10 @@
+package io.thoughtworksarts.riot.branching.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Intro {
+    @Getter @Setter private int intro;
+    @Getter @Setter private String start;
+    @Getter @Setter private String end;
+}
