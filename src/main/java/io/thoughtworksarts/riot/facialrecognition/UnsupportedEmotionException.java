5c11263fe8fcbf685cb0170e9d203e2476ae6430
angelicaperez37 <angelica.perez37@gmail.com>
2018-04-11 17:46:57 -0400

    Remove unused exception class

--- src/main/java/io/thoughtworksarts/riot/facialrecognition/UnsupportedEmotionException.java
+++
@@ -1,9 +0,0 @@
-package io.thoughtworksarts.riot.facialrecognition;
-
-public class UnsupportedEmotionException extends Exception{
-
-    public UnsupportedEmotionException(String emotion) {
-        super(String.format("The emotion %s is not supported by this instance of FacialEmotionRecognitionAPI", emotion));
-    }
-
-}
