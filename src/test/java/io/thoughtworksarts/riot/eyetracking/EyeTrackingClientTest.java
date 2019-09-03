917e85ef837fdad598f8d00efbaf86ad216902b4
Emilio Escobedo <emilio.escobedo@thoughtworks.com>
2019-09-03 10:35:34 -0400

    Manual tests for EyeTrackingClient

--- src/test/java/io/thoughtworksarts/riot/eyetracking/EyeTrackingClientTest.java
+++ src/test/java/io/thoughtworksarts/riot/eyetracking/EyeTrackingClientTest.java
@@ -1,5 +1,36 @@
+package io.thoughtworksarts.riot.eyetracking;
+
+import io.thoughtworksarts.riot.branching.model.Level;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EyeTrackingClientTest {
+
+    @Test
+    public void test1() {
+        EyeTrackingClient eyeTrackingClient = new EyeTrackingClient();
+        eyeTrackingClient.startEyeTracking("something different");
+    }
+
+    @Test
+    public void test2() {
+        EyeTrackingClient eyeTrackingClient = new EyeTrackingClient();
+        eyeTrackingClient.stopEyeTracking("hue");
+    }
+
+    @Test
+    public void test3() {
+        Level level = new Level();
+        level.setLevel(1);
+        level.setStart("hue");
+        level.setEnd("endhue");
+        ArrayList levels = new ArrayList();
+        levels.add(level);
+        EyeTrackingClient eyeTrackingClient = new EyeTrackingClient();
+        eyeTrackingClient.createEyeTrackingVisualization(levels);
+    }
 
 }
