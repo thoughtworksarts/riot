2adf6965b698b7ab4f5d724bdd928218938a34b9
Stephanie Weber <sweber@thoughtworks.com>
2019-09-14 13:50:00 -0700

    Call calibration script during application initialization

---
+++ src/main/java/io/thoughtworksarts/riot/utilities/test1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# TODO: Remove this test file when actual calibration is in place
+def calibrate(number):
+    print("Initiating calibration...")
+    print("Calibration process concluded")
+    exit(0)
+
+
+calibrate(0)
